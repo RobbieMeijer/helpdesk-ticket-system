ee22e8dd690aec281b6151fd0b29419f7a997072
Robbie Meijer <github.rro88ie@xs4all.nl>
2021-05-02 15:25:13 +0200

    adding new Sidebar component

---
+++ helpdesk-ticket-system/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Tickets</li>
+          <li>Customers</li>
+          <li>Reports</li>
+          <li>Knowledge base</li>
+          <li>Search</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
