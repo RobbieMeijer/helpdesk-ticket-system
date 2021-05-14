bb4b4781207c63ce6360f9dfeff9b4aa7ad492ff
Robbie Meijer <github.rro88ie@xs4all.nl>
2021-05-14 14:06:03 +0200

    change some bootstrap col

--- helpdesk-ticket-system/src/components/App.js
+++ helpdesk-ticket-system/src/components/App.js
@@ -12,10 +12,10 @@ const App = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
+        <div className="col-3">
           <Sidebar />
         </div>
-        <div className="col">
+        <div className="col-9">
           <Route path="/">
             <Dashboard />
           </Route>
