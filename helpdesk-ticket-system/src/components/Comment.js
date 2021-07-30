b9afe6cd7ff8d473c5e0ec3ec8e4a60480881f0a
Robbie Meijer <github.rro88ie@xs4all.nl>
2021-07-30 15:40:59 +0200

    add: initial setup comment component

---
+++ helpdesk-ticket-system/src/components/Comment.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Comment = () => {
+  return (
+    <article>
+      <h3>Reporter Name 32 seconds ago</h3>
+      <p>
+        Fusce molestie aliquam nunc, non ornare diam fringilla quis. Quisque
+        consectetur non nunc accumsan vestibulum. Sed aliquam tristique magna
+        non lobortis. Duis posuere, ligula nec suscipit tempus, orci augue
+        sollicitudin leo, vel bibendum nibh augue bibendum urna. Donec vitae
+        pretium tortor. Pellentesque habitant morbi tristique senectus et netus
+        et malesuada fames ac turpis egestas. Aenean vel condimentum felis.
+      </p>
+      <div>
+        <button>Edit</button> <button>Delete</button>
+      </div>
+    </article>
+  );
+};
+
+export default Comment;
